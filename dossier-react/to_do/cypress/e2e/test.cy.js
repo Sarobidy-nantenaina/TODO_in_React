c2a24513bb7d1f6801832ba94a99473acd6cec70
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2023-02-28 23:46:09 +0300

    feat:  test-react-cypress

--- dossier-react/to_do/cypress/e2e/test.cy.js
+++ dossier-react/to_do/cypress/e2e/test.cy.js
@@ -5,4 +5,52 @@ describe('template spec', () => {
     cy.contains('children').click();
     cy.url().should('contains','traversal')
   })
+
+  it('visit localhost', () => {
+    cy.visit('http://localhost:3000')
+  })
 })
+
+describe('Ajouter une tâche', () => {
+  it('Ajoute une tâche à la liste todo', () => {
+    // Visite l'URL de l'application
+    cy.visit('http://localhost:3000');
+
+    // Sélectionne le champ de texte et saisit une nouvelle tâche
+    cy.get('input[type="text"]').type('Nouvelle tâche');
+
+    // Appuie sur la touche "Entrée" pour ajouter la tâche
+    cy.get('input[type="text"]').type('{enter}');
+
+    // Vérifie que la tâche a bien été ajoutée à la liste todo
+    cy.get('.list-item')
+      .should('have.length', 1)
+      .and('contain', 'Nouvelle tâche');
+  });
+
+  it('moves task to done when checkbox is checked', () => {
+   // Visite l'URL de l'application
+   cy.visit('http://localhost:3000');
+
+   // Sélectionne le champ de texte et saisit une nouvelle tâche
+   cy.get('input[type="text"]').type('Nouvelle tâche');
+
+   // Appuie sur la touche "Entrée" pour ajouter la tâche
+   cy.get('input[type="text"]').type('{enter}');
+
+   // Vérifie que la tâche a bien été ajoutée à la liste todo
+   cy.get('.list-item')
+     .should('have.length', 1)
+     .and('contain', 'Nouvelle tâche');
+    
+    // Coche la case de la tâche
+    cy.get('input[type="checkbox"]').check();
+    
+    // Vérifie que la tâche a été déplacée dans le tableau "done"
+    cy.get('.list-item')
+      .should('have.length', 1)
+      .and('contain', 'Nouvelle tâche');
+  });
+  
+
+});
